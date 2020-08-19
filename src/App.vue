cf0adddea0f0357d65d329a33192c628ec3dc805
Rinat Khusainov <khusainovrm@yandex.ru>
2020-08-19 21:31:31 +0300

    commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Main />
+  </div>
+</template>
+
+<script>
+import Main from './views/Main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+@import "assets/index.css";
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div id="pentagon"></div>
+
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+#pentagon {
+  position: relative;
+	width: 320px;
+	height: 480px;
+  box-sizing: content-box;
+  border-width: 50px 18px 0;
+  border-style: solid;
+  border-color: red transparent;
+}
+#pentagon:before {
+  content: "";
+  position: absolute;
+  height: 0;
+  width: 0;
+  top: -85px;
+  left: -18px;
+  border-width: 0 45px 35px;
+  border-style: solid;
+  border-color: transparent transparent red;
+}
+  
+
+</style>
